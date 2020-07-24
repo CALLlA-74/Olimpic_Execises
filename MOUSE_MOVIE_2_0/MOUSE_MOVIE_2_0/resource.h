df88fe265b21f0ee5b1e16fdae9174d857d572d2
CALLlA-74 <56507954+CALLlA-74@users.noreply.github.com>
2020-07-24 12:00:02 +0500

    fff

---
+++ MOUSE_MOVIE_2_0/MOUSE_MOVIE_2_0/resource.h
@@ -0,0 +1,52 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется MOUSE_MOVIE_2_0.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MOUSEMOVIE20_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MOUSEMOVIE20                107
+#define IDI_SMALL                       108
+#define IDC_MOUSEMOVIE20                109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_MOVE_CURSOR                  32772
+#define ID_32773                        32773
+#define ID_STOP                         32774
+#define ID_START                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32783                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_2                            32786
+#define ID_1                            32787
+#define ID_3                            32788
+#define ID_4                            32789
+#define ID_5                            32790
+#define ID_6                            32791
+#define ID_7                            32792
+#define ID_8                            32793
+#define ID_9                            32794
+#define ID_10                           32795
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32796
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
